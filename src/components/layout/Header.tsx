a661e32b936fdc94a7d8c4258d634478c91b30b1
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2023-12-27 17:49:32 +0100

    feat: implement and style menu in moble and desktop

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,39 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Nav } from "./Nav";
 import HamburgerMenu from "react-hamburger-menu";
-// import {
-//   BrowserView,
-//   MobileView,
-//   isBrowser,
-//   isMobile,
-// } from "react-device-detect";
+import { Link, useLocation } from "react-router-dom";
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [showNav, setShowNav] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setShowNav(false);
+    setIsOpen(false);
+  }, [location]);
+
   const handleMenuClick = () => {
+    setShowNav(!showNav);
     setIsOpen(!isOpen);
   };
   return (
     <>
       <div className="container-column header">
         <div className="top">
-          <div className="logo">Brand</div>
-          {!isOpen ? (
-            <button
-              typeof="button"
-              className="menu"
-              onClick={() => {
-                setShowNav(!showNav);
-              }}
-            >
-              Menu
-              <span className="material-symbols-rounded">
-                keyboard_arrow_down
-              </span>
-            </button>
-          ) : null}
+          <div className="logo">
+            <Link to={"/"}>RE:LIFE</Link>
+          </div>
+          <div className="icon-container">
+            <Link to={"/profile"}>
+              <span className="material-symbols-rounded">favorite</span>
+            </Link>
+            <Link to={"/profile"}>
+              <span className="material-symbols-outlined">person</span>
+            </Link>
+            {!isOpen ? (
+              <button
+                typeof="button"
+                className="menu"
+                onClick={() => {
+                  setShowNav(!showNav);
+                }}
+              >
+                Menu
+                <span className="material-symbols-rounded">
+                  keyboard_arrow_down
+                </span>
+              </button>
+            ) : null}
+          </div>
         </div>
+        <div className="bottom"></div>
 
         <div className="hamburger">
           <HamburgerMenu
@@ -44,7 +58,6 @@ export const Header = () => {
             height={20}
           ></HamburgerMenu>
         </div>
-        {isOpen ? <Nav></Nav> : null}
         {showNav ? <Nav></Nav> : null}
       </div>
     </>
