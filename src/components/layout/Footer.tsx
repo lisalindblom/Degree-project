880ae181bdcf5acb7f9c8da4e73439f17d5a95c7
Lisa Lindblom <113437951+lisalindblom@users.noreply.github.com>
2024-01-08 14:55:22 +0100

    Feat/footer (#8)
    * feat: create footer
    
    * build: add bootstrap icons
    
    * fix: filter styling errors
    
    ---------
    
    Co-authored-by: lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,30 +1,48 @@
+import group from "../../assets/footergroup.png";
+import lady from "../../assets/footerlady.png";
+
 export const Footer = () => {
   return (
     <>
-      <footer className="outline flex-col footer">
-        <div id="contact">
-          Vill du se fler av mina projekt eller komma i kontakt med mig?
+      <footer className="footer">
+        <div className="footer-top">
+          <div className="text">
+            <h3>Want to contribute?</h3>
+            <p>
+              We are happily accepting contributions from our creative
+              community. It could be anything from a guide or inspirational post
+              to an idea. We would love to hear from you!
+            </p>
+          </div>
+
+          <div className="graphics">
+            <img className="group" src={group} alt="" />
+            <img className="lady" src={lady} alt="" />
+          </div>
         </div>
-        <div id="contact__links">
-          <div className="mail">
-            <a href="mailto: lisa.manssonlindblom@medieinstitutet.se">
-              <i className="bi bi-envelope"></i>
-              lisa.manssonlindblom@medieinstitutet.se
-            </a>
+        <div className="footer-bottom">
+          <div className="contact">
+            <h3>Contact:</h3>
+            <div className="mail">
+              <a href="mailto: lisa.manssonlindblom@medieinstitutet.se">
+                mend@mail.com
+              </a>
+            </div>
           </div>
-
-          <div className="github">
-            <a href="https://github.com/lisalindblom">
-              <i className="bi bi-github"></i>
-              lisalindblom
-            </a>
-          </div>
-
-          <div className="linkedin">
-            <a href="https://www.linkedin.com/in/lisa-lindblom-49940b254/">
-              <i className="bi bi-linkedin"></i>
-              lisa-lindblom-49940b254
-            </a>
+          <div className="social">
+            <h3>Follow us:</h3>
+            <div className="icons">
+              <div className="github">
+                <a href="https://github.com/lisalindblom">
+                  <i className="bi bi-github"></i>
+                </a>
+              </div>
+              <div className="instagram">
+                <a href="https://github.com/lisalindblom">
+                  <i className="bi bi-instagram"></i>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </footer>
