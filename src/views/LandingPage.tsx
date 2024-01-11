b423a11b5996189e7df18b6e278120fa3e756bf1
Lisa Lindblom <113437951+lisalindblom@users.noreply.github.com>
2024-01-11 11:41:03 +0100

    Feat/startpage (#10)
    * feat: create startpage
    
    * style: add styling for startpage and create mixin for to-top button
    
    * style: add images
    
    * style: implement responsiveness
    
    * style: textstyling
    
    ---------
    
    Co-authored-by: lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -1,7 +1,78 @@
+import { Link } from "react-router-dom";
+import needle from "../assets/sewing-needle.png";
+import leaf from "../assets/leaf.png";
+import earth from "../assets/earth.png";
+import { useEffect, useState } from "react";
 export const LandingPage = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.scrollY > 200) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
   return (
     <>
-      <div className="outline landing main-content">landing page</div>
+      <div className="startpage">
+        <div className="hero">
+          <h1>Embracing sustainability, one stitch at a time</h1>
+        </div>
+        <div className="content-container">
+          <div className="content-card">
+            <img src={needle} alt="" />
+            <h2>Explore creative guides</h2>
+            <p>
+              Dive into a world of DIY fashion! Browse guides on repairing,
+              remaking, and reusing clothing.
+            </p>
+            <Link to="/posts">
+              Read more<i className="bi bi-arrow-right"></i>
+            </Link>
+          </div>
+          <div className="content-card">
+            <img src={leaf} alt="" />
+            <h2>Fabric insights</h2>
+            <p>
+              Uncover the different fabrics, their characteristics, and how to
+              care for them.
+            </p>
+            <Link to="/material-guide">
+              Read more<i className="bi bi-arrow-right"></i>
+            </Link>
+          </div>
+          <div className="content-card">
+            <img src={earth} alt="" />
+            <h2>Environmental Impact</h2>
+            <p>
+              Explore our resources that shed light on the environmental
+              consequences of our clothing consumption.
+            </p>
+            <Link to="/climate-impact">
+              Read more<i className="bi bi-arrow-right"></i>
+            </Link>
+          </div>
+        </div>
+        {isVisible && (
+          <button className="top-of-page" onClick={scrollToTop}>
+            <i className="bi bi-arrow-up"></i>
+          </button>
+        )}
+      </div>
     </>
   );
 };
